ccb5d4663fcbe2e5edbf8b494beb5224ad2ecece
umairullah <umair.ullah@globalnorthstar.com>
2017-07-14 18:49:12 +0500

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestool;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		SalesData salesData = new SalesData();
+		
+		displayGreeting();
+		salesData.display();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This App shows sales data");
+	}
+}
